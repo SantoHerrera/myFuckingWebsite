d01e7938313fac40d4d680b8fbb7c1963ecdea7b
Rafael Herrera <santoElias65537@gmail.com>
2021-12-05 22:24:31 -0700

    its good now

--- src/mock/data.js
+++ src/mock/data.js
@@ -29,33 +29,33 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'clojuretictac.png',
-    title: 'Clojure Tic Tac Toe',
+    img: 'nuova.png',
+    title: 'Professional Experience',
     info:
-      "When trying to learn a new framework/language, recreating tic tac toe is my goto. This will usually be enough to familiarize with syntax, and some of the quirks that the framework/language has. Out of all the versions i've made the clojure version has been my favorite. Why? Mostly becuase it forced me into doing things differently. How so? It made me code in a functional way. I also learned how to recursively pass state around. Which I think is a neat way of doing things.",
-    info2: '',
-    url: 'https://github.com/SantoHerrera/clojuretictac',
-    repo: 'https://github.com/SantoHerrera/clojuretictac', // if no repo, the button will not show up
+      "Heres a website I made for a client. I got a react template I found on github and changed it to thier needs. Although it's very similer to the original I did run into a bunch of roadblocks changing it.",
+    info2: 'Original: https://github.com/Dorota1997/react-frontend-dev-portfolio',
+    url: 'https://nuovaimmagine.netlify.app/',
+    repo: 'https://github.com/SantoHerrera/nuovaimmagine', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'Screenshot (7).png',
     title: 'Vue Minesweeper ',
     info:
-      "I love minesweeper. It was actually the thing that got me into programming. I've rewritten this alot aswell. My favorite version has been the Vue js version. Why? Becuase its the shortest version. ",
+      "I love minesweeper, it was the thing that got me into programming. I've rewritten this alot. My favorite version being the Vue js version as its the shortest version. ",
     info2: '',
     url: 'https://vueminesweeper.netlify.app/',
     repo: 'https://github.com/SantoHerrera/minesweeperbutinvue', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'nuova.png',
-    title: 'Professional Experience',
+    img: 'clojuretictac.png',
+    title: 'Clojure Tic Tac Toe',
     info:
-      "Heres a website I made for a client. I didn't make this from scratch. I got a premade template and changed it to thier needs. They were really happy with the results. As was I. Although it's very similer to the original template I did run into a bunch of roadblocks changing it to my needs.",
-    info2: 'Original: https://github.com/Dorota1997/react-frontend-dev-portfolio',
-    url: 'https://nuovaimmagine.netlify.app/',
-    repo: 'https://github.com/SantoHerrera/nuovaimmagine', // if no repo, the button will not show up
+      "When trying to learn a new framework/language, recreating tic tac toe is my goto. This will usually get the ball rolling with syntax and everything that follows. Out of all the versions i've made the clojure version has been my favorite. As it forced me into doing things in a functional way.",
+    info2: '',
+    url: 'https://github.com/SantoHerrera/clojuretictac',
+    repo: 'https://github.com/SantoHerrera/clojuretictac', // if no repo, the button will not show up
   },
 ];
 
